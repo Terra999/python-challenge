54906aa6dea1e9f5a30276fe9b8cc6400bf1397a
Terra Vaughn <vterra28@yahoo.com>
2021-01-22 21:47:28 -0600

    completed most of PyPoll with a lot of help

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,7 @@
+# Dear Grader - I'll be honest - most of this code was made possible because of help 
+# from the following people: Stephanie Richards - she posted her code(!), Beau Jeffrey, Melissa Lowe and as always Sharon Templin.
+# I understand things in small chunks, I just don't get how they all go together. I'll keep trying though!
+
 # Import modules
 import os
 import csv
@@ -9,14 +13,13 @@ print(csvpath)
 # Open and read csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
     # print(csvreader)
 
     # Read the header row first
     csv_header = next(csvreader)
     print(f"Header: {csv_header}")
 
-    # Identify variables
+    # Define variables
     total_votes = 0
     candidate_name = []
     
@@ -30,21 +33,18 @@ with open(csvpath) as csvfile:
         # Where to find candidate names to put inside the list
         candidate_name.append(str(row[2]))
 
-
 # Define variables
 unique_candidates = []
 candidate_votes = []
 vote_count = 0
 
-# Set up a function to find unique
+# Set up a function to find unique candidate name
 def unique(candidate_name):
     
     # Read through each row
     for name in candidate_name:
-
          if name not in unique_candidates:
                 unique_candidates.append(name)
-
     return unique_candidates
 
 # Return unique candidate names
@@ -55,38 +55,47 @@ unique(candidate_name)
 def vote_count(unique_candidates):
     # Set counter to 0
     vote_count = 0
-    # 
+
+    # Go row by row looking for candidate name
     for name in unique_candidates:
+        # Then compare to find number of votes
         for vote in candidate_name:
+            #If they match up then add to vote count
             if name == vote:
                 vote_count = int(vote_count) + 1
+        # Add to vote_count list
         candidate_votes.append(vote_count)
+        # Reset counter
         vote_count = 0
     return candidate_votes
 
 vote_count(unique_candidates)
-print(candidate_votes)
+# print(candidate_votes)
 
-# def print_percentages(candidate_votes):
+# Find the winner
+win_vote = max(candidate_votes)
+win_index = candidate_votes.index(win_vote)
+winner = unique_candidates[win_index]
+# print(winner)
 
-#     candidate_votes = 0
-#     unique_candidates = 0 
+# Find candidate vote percent - I almost figured this out by myself!
+candidate_vote_percent = []
+for x in (candidate_votes):
+    vote_percent = round((float(int(x) / int(total_votes)) * 100), 3)
+    candidate_vote_percent.append(vote_percent)
+# print(candidate_vote_percent)
 
-#     win_percent = round(candidate_votes / unique_candidates * 100, 2)
-
-#     print(win_percent)
-
-
-
-
+#zip lists to make tuple - Yay! I actually understand the tuple part! Needed help printing the candidate_tuple below though.
+candidate_tuple = tuple(zip(unique_candidates, candidate_vote_percent, candidate_votes))
         
 
-        
-
-        
-
-
+print(" ")
 print("Election Results")       
 print("---------------------------------")
 print(f"Total Votes: {total_votes}")
+print("---------------------------------")
+for lst in candidate_tuple:
+    print(f"{lst[0]}: {lst[1]}00% ({lst[2]})")
+print("---------------------------------")
+print(f"Winner: {winner}")
 print("---------------------------------")
